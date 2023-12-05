516175979f70d337f39e0c7288437a742603ad05
Lucho <lucho@MacBook-Pro-de-Luciano.local>
2023-12-05 17:34:40 -0300

    Fixed test

--- src/components/Switch/index.spec.tsx
+++ src/components/Switch/index.spec.tsx
@@ -14,22 +14,21 @@ describe('Switch component', () => {
     expect(rightButton).toBeInTheDocument();
   });
 
-  it('calls onSwitch with correct option when left button is clicked', () => {
+  it('calls onSwitch with correct option when buttons are clicked', () => {
     const onSwitchMock = jest.fn();
     const { getByText } = render(<Switch leftOption="Left" rightOption="Right" onSwitch={onSwitchMock} />);
     const leftButton = getByText('Left');
-
-    fireEvent.click(leftButton);
-    expect(onSwitchMock).toHaveBeenCalledWith('Left');
-  });
-
-  it('calls onSwitch with correct option when right button is clicked', () => {
-    const onSwitchMock = jest.fn();
-    const { getByText } = render(<Switch leftOption="Left" rightOption="Right" onSwitch={onSwitchMock} />);
     const rightButton = getByText('Right');
+
+    // first time "Left" is selected by default so it don't call onSwitch
+    fireEvent.click(leftButton);
+    expect(onSwitchMock).not.toHaveBeenCalled();
 
     fireEvent.click(rightButton);
     expect(onSwitchMock).toHaveBeenCalledWith('Right');
+
+    fireEvent.click(leftButton);
+    expect(onSwitchMock).toHaveBeenCalledWith('Left');
   });
 
   it('applies selected styles to the clicked button', () => {
