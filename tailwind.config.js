9de8c8017707dd9c4b64be12013fadfa1cea32d2
Hernan Case <hernan.case@deptagency.com>
2023-12-13 12:33:42 -0300

    add Colors and Font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import { colors } from './src/constants'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        maison: ['Maison Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      colors,
+    },
   },
   plugins: [],
 }
