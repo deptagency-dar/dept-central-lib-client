18925f3ebdb1436415203f2db562135f6808c9e3
Dario Emmanuel Gonzalez Bano <darioegb@gmail.com>
2024-02-15 16:54:33 -0300

    Feature/add datepicker component (#9)
    * add datepicker component
    
    * fix problem with postcss compilation

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import './styles/styles.css'
 export {
   Button,
   Checkbox,
+  DatePicker,
   Radio,
+  Switch,
   TextField,
+  SearchInput,
+  Select,
   type ButtonProps,
   type CheckboxProps,
+  type DatePickerProps,
   type RadioProps,
+  type SwitchProps,
   type TextFieldProps,
+  type SearchInputProps,
+  type SelectProps,
 } from './components'
