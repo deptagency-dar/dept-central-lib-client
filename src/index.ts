27a666389583702a2e1cbba090022d5980ab34ed
Juan Araneta <juanaraneta10@gmail.com>
2024-03-12 10:27:24 -0300

    feat(Global): fix export

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,2 @@
 import '../globals.css'
-
-
-export {
-  Button,
-  Checkbox,
-  DatePicker,
-  Radio,
-  Switch,
-  TextField,
-  SearchInput,
-  Select,
-  type ButtonProps,
-  type CheckboxProps,
-  type DatePickerProps,
-  type RadioProps,
-  type SwitchProps,
-  type TextFieldProps,
-  type SearchInputProps,
-  type SelectProps,
-} from './components'
+export * from './components'
