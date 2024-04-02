c6de182836499c6541b284a77f063ffe51fcb653
user <matias.blanco@deptagency.com>
2024-04-02 19:43:30 +0200

    feat: new table component

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,79 @@
+import { Fragment, HTMLAttributes, ReactElement, forwardRef } from 'react'
+
+interface TableOwnProps {
+  head: HeadProps
+  body: BodyProps
+}
+
+interface HeadProps {
+  headers?: Array<string>
+  className?: string
+  headersComponent?: Array<ReactElement>
+}
+
+interface BodyProps {
+  bodyComponent: Array<Array<ReactElement | string | (ReactElement & string)>>
+  className?: string
+}
+
+type TablePropsRootAttributes = Pick<
+  HTMLAttributes<HTMLTableElement>,
+  'className' | 'style' | 'id'
+>
+
+type TableProps = TablePropsRootAttributes & TableOwnProps
+
+export const Table = forwardRef<HTMLTableElement, TableProps>(
+  ({ className = '', body, head, ...rest }, ref) => {
+    return (
+      <div>
+        <table className={`table-auto ${className}`} ref={ref} {...rest}>
+          <thead>
+            <tr>
+              {head.headers &&
+                head.headers.map((header, index) => {
+                  return (
+                    <th
+                      key={`header-${index}`}
+                      className="px-4 py-6 border-b border-b-gray-200 text-left"
+                    >
+                      <span className="text-gray-500 font-normal ">
+                        {header}
+                      </span>
+                    </th>
+                  )
+                })}
+              {head.headersComponent &&
+                head.headersComponent.map((header, index) => {
+                  return <Fragment key={`header-${index}`}>{header}</Fragment>
+                })}
+            </tr>
+          </thead>
+          <tbody>
+            {body.bodyComponent.map((items, index) => {
+              return (
+                <tr
+                  key={`tr-item-${index}`}
+                  className="border-b border-b-gray-200"
+                >
+                  {items.map((item, subIndex) => {
+                    return (
+                      <td
+                        className="px-4 py-8 text-left"
+                        key={`item-${subIndex}`}
+                      >
+                        {item}
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  },
+)
+
+Table.displayName = 'Table'
