0586e3e81eb40b651966fdc1c267f821fe52ab08
Leandro Bisceglie <leandro.bisceglie@MacBook-Pro-de-Leandro.local>
2024-04-17 19:58:45 -0300

    feat(pagination): add responsive styles

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -18,46 +18,67 @@ export const Pagination: FC<PaginationProps> = ({
 }) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage)
 
+  const getPageRange = () => {
+    const pages = []
+    let i = 1
+
+    while (i <= totalPages) {
+      if (
+        i <= 1 ||
+        i === totalPages ||
+        (i >= currentPage - 1 && i <= currentPage + 1)
+      ) {
+        pages.push(i)
+        i++
+      } else {
+        pages.push('...')
+
+        if (i < currentPage - 1) {
+          i = currentPage - 1
+        } else {
+          i = totalPages;
+        }
+      }
+    }
+    return pages
+  }
+
   return (
-    <div
-      className={`flex items-center justify-center gap-4 justify-between ${className}`}
-      data-testid="pagination-container"
-    >
+    <div className={`flex items-center justify-center gap-1 justify-between sm:gap-4 ${className}`} data-testid="pagination-container">
       <button
         onClick={() => onPageChange(Math.max(1, currentPage - 1))}
         disabled={currentPage === 1}
-        className={`inline-flex gap-1 items-center justify-center w-28 h-8 rounded-full text-base font-semibold ${currentPage === 1 ? 'cursor-not-allowed text-gray-400 bg-white' : 'text-violet-800 hover:text-white bg-white hover:bg-indigo-500'} border-2 ${currentPage === 1 ? 'border-gray-300' : 'border-violet-700 hover:border-transparent'} focus:outline-none`}
+        className="inline-flex gap-1 items-center justify-center min-sm:w-20 h-6 sm:w-28 sm:h-8 rounded-full text-sm sm:text-base font-semibold cursor-pointer text-violet-800 hover:text-white bg-white hover:bg-indigo-500 border-violet-700 hover:border-transparent focus:outline-none disabled:cursor-not-allowed disabled:text-gray-400 disabled:bg-white disabled:border-gray-300"
       >
         <ArrowLeftIcon className="w-4 h-4" aria-hidden="true" />
-        Previous
+        <span className="hidden sm:inline">Previous</span>
       </button>
 
-      <div className="flex gap-2">
-        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-          <button
-            key={page}
-            aria-current={page === currentPage ? 'page' : undefined}
-            className={`w-8 h-8 flex items-center justify-center rounded-full text-base font-semibold ${page === currentPage ? 'text-purple-800' : 'text-gray-700'} ${page === currentPage ? '' : 'hover:bg-purple-500'} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500`}
-            onClick={() => onPageChange(page)}
-            disabled={page === currentPage}
-            style={{
-              backgroundColor:
-                page === currentPage ? 'rgba(219, 211, 254, 1)' : 'transparent',
-            }}
-          >
-            {page}
-          </button>
-        ))}
+      <div className="flex gap-1 sm:gap-2 items-center">
+        {getPageRange().map((page, index) => 
+          typeof page === 'number' ? (
+            <button
+              key={index}
+              className={`w-6 h-6 sm:w-8 sm:h-8 flex items-center justify-center rounded-full text-xs sm:text-base font-semibold ${page === currentPage ? 'text-purple-800 bg-indigo-200' : 'text-gray-700 hover:bg-purple-500'} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500`}
+              onClick={() => onPageChange(page)}
+              disabled={page === currentPage}
+            >
+              {page}
+            </button>
+          ) : (
+            <span key={index} className="text-gray-500 flex items-center justify-center text-xs sm:text-base">...</span>
+          )
+        )}
       </div>
 
       <button
         onClick={() => onPageChange(Math.min(totalPages, currentPage + 1))}
         disabled={currentPage === totalPages}
-        className={`inline-flex items-center justify-center w-28 h-8 rounded-full text-base font-semibold ${currentPage === totalPages ? 'cursor-not-allowed text-gray-400 bg-white' : 'text-violet-800 hover:text-white bg-white hover:bg-indigo-500'} border-2 ${currentPage === totalPages ? 'border-gray-300' : 'border-violet-700 hover:border-transparent'} focus:outline-none`}
+        className="inline-flex items-center justify-center min-sm:w-20 h-6 sm:w-28 sm:h-8 rounded-full text-sm sm:text-base font-semibold cursor-pointer text-violet-800 hover:text-white bg-white hover:bg-indigo-500 border-violet-700 hover:border-transparent focus:outline-none disabled:cursor-not-allowed disabled:text-gray-400 disabled:bg-white disabled:border-gray-300"
       >
-        Next
+        <span className="hidden sm:inline">Next</span>
         <ArrowRightIcon className="w-4 h-4 ml-2" aria-hidden="true" />
       </button>
     </div>
-  )
+  );
 }
