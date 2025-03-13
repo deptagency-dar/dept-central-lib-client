1f73a23a37cbc96314c8dd747cd19badde9a4354
Hernan Case <hercase92@gmail.com>
2025-03-13 17:36:24 -0300

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,13 @@ import { theme } from './src/constants'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
         maison: ['Maison Neue', 'Helvetica', 'Arial', 'sans-serif'],
       },
-      colors: theme.colors,
+      ...theme,
     },
   },
   plugins: [],
