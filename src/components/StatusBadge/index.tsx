967042018fcf2355fe07fe3f1d5bbff51d699a89
Keis <hercase92@gmail.com>
2025-04-11 12:03:51 -0300

    Fix/use datepicker (#82)

--- src/components/StatusBadge/index.tsx
+++ src/components/StatusBadge/index.tsx
@@ -1,34 +1,19 @@
-import { forwardRef, HTMLAttributes } from 'react'
 import styles from './index.module.css'
-type StatusBadgePropsRootAttributes = Pick<
-  HTMLAttributes<HTMLDivElement>,
-  'className' | 'style' | 'id'
->
 
-interface StatusBadgeOwnProps {
+interface StatusBadgeProps {
   type: 'pending' | 'denied' | 'approved' | 'reminded' | 'archived' | 'resolved'
   text?: string | null
 }
 
-export type StatusBadgeProps = StatusBadgePropsRootAttributes & StatusBadgeOwnProps
-
-export const StatusBadge = forwardRef<HTMLDivElement, StatusBadgeProps>(
-  ({ className = '', type, text = null, ...rest }, ref) => {
-    return (
+export const StatusBadge = ({ type, text = null }: StatusBadgeProps) => {
+  return (
+    <div className="flex flex-row items-center pl-2 py-2 pr-3 rounded-full h-6 w-fit text-nowrap ${className} ${styles[type]}">
       <div
-        ref={ref}
-        className={`flex flex-row items-center pl-2 py-2 pr-3 rounded-full h-6 w-fit text-nowrap ${className} ${styles[type]}`}
-        {...rest}
-      >
-        <div
-          className={`bg-red w-2 h-2 mr-2 rounded-full dot-${type} ${styles[`dot-${type}`]}`}
-        ></div>
-        <span className={`text-sm font-semibold ${styles[`text-${type}`]}`}>
-          {text || type.toUpperCase()}
-        </span>
-      </div>
-    )
-  },
-)
-
-StatusBadge.displayName = 'StatusBadge'
+        className={`bg-red w-2 h-2 mr-2 rounded-full dot-${type} ${styles[`dot-${type}`]}`}
+      ></div>
+      <span className={`text-sm font-semibold ${styles[`text-${type}`]}`}>
+        {text || type.toUpperCase()}
+      </span>
+    </div>
+  )
+}
